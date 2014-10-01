7842264dd7185abe5afa90101dd7861a34c75978
Paul Carvill <paul@paulcarvill.com>
2014-10-01 13:23:21 +0100

    Initial Commit

---
+++ ApprenticeBullseye/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ApprenticeBullseye
+//
+//  Created by paul on 01/10/2014.
+//  Copyright (c) 2014 paulcarvill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ ApprenticeBullseye/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  ApprenticeBullseye
+//
+//  Created by paul on 01/10/2014.
+//  Copyright (c) 2014 paulcarvill. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
